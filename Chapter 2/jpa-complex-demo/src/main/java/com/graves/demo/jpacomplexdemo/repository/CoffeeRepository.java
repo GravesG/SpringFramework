8967f384c72cddfecbd5a0ab8407255ffc403063
GravesG <18870224341@163.com>
2020-03-30 23:54:26 +0800

    JPA相关

---
+++ Chapter 2/jpa-complex-demo/src/main/java/com/graves/demo/jpacomplexdemo/repository/CoffeeRepository.java
@@ -0,0 +1,7 @@
+package com.graves.demo.jpacomplexdemo.repository;
+
+
+import com.graves.demo.jpacomplexdemo.model.Coffee;
+
+public interface CoffeeRepository extends BaseRepository<Coffee, Long> {
+}
